e44aed1036f3dbff157f9dbfd927919028eb7311
Stefan Baumgartner <sbaumg@gmail.com>
2022-02-23 15:42:29 +0100

    add customhandler port

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,15 +4,24 @@ use app::{handler, Error};
 use http_layer::HttpParserLayer;
 use hyper::{service::make_service_fn, Server};
 
-use std::result;
+use std::net::SocketAddr;
 use std::{convert::Infallible, time::Duration};
+use std::{env, result};
 use tower::{timeout::TimeoutLayer, ServiceBuilder};
 
 type Result<T> = result::Result<T, Error>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let addr = "127.0.0.1:1337".parse().unwrap();
+    let port_key = "FUNCTIONS_CUSTOMHANDLER_PORT";
+    let addr = "127.0.0.1".parse().unwrap();
+
+    let port: u16 = match env::var(port_key) {
+        Ok(val) => val.parse().expect("Custom handler port is not a number"),
+        Err(_) => 1337,
+    };
+
+    let addr = SocketAddr::new(addr, port);
 
     let make_service = make_service_fn(|_conn| async {
         let service = ServiceBuilder::new()
